29e2a0b3b6f02d19af707c19ec5b03a0cb4ece95
lari <102737242+ariwd0903@users.noreply.github.com>
2022-05-23 09:11:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+x=1
+while (x<=100){
+    console.log (x)
+    x= x+1
+}
+
+y=50
+while (y<=100){
+    console.log (y)
+    y= y+1
+}
+
+w=10
+while (w>=0){
+    console.log (w)
+ w= w-1
+}
+a=0 
+while (a<=20){
+    console.log(a)
+    a= a + 2
+}
